03c49e1784bb7e8f9c2f7f160b4ea223a0526042
Rima Bhumbla <105192284+rbhumbla1@users.noreply.github.com>
2022-11-09 20:14:54 -0800

    finished project

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,36 +1,90 @@
 var timeRem = document.getElementById('timeRem');
 var wordTxt = document.getElementById('word');
+var statsTxt = document.getElementById('stats');
+var timerBtn = document.querySelector("#timerBtn");
+var winner = document.querySelector("h3");
+var timeLeft = 5;
 var wLen = 0;
+var gTries = 0;
+var gWins = 0;
+var gLosses = 0;
 
-var word = "javascript"; //hard coding for testing
+var word = "string"; //hard coding for testing
+var wArr = word.split(''); //array of words characters
+var kArr = [];
 
+function countdown(event) {
 
-function countdown() {
-    var timeLeft = 5;
+  event.stopPropagation();
+
+  //increment tries count
+  gTries++;
+  kArr = Array(wArr.length).fill('-');
+  wordTxt.textContent = (kArr.join(""));
+
+  winner.textContent = "";
   
-    // TODO: Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
-    var timeInterval = setInterval(function () {
-      //
-      // YOUR CODE HERE
-      //
+  // TODO: Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
+  var timeInterval = setInterval(function () {
+
+    if (timeLeft === 0) {
+      console.log("timer done");
+      // Stops execution of action at set interval
+      clearInterval(timeInterval);
+      timeLeft = 5;
+      timeRem.textContent = "Done. Start timer for another round.";
+      // Calls function to update stats
+      displayResults();
+    } else {
+      console.log("timer left");
       timeLeft--;
-      timeRem.textContent = timeLeft + " seconds remaining.";
+    timeRem.textContent = "Start typing. " + timeLeft + " seconds remaining.";
+      getUserInput();
+    }
+  }, 1000);
+}
+
+function displayResults() {
+  console.log("in displayResults");
+
+  if(kArr.join('') === word){
+    gWins++;
   
-      if (timeLeft === 0) {
-        // Stops execution of action at set interval
-        clearInterval(timeInterval);
-        // Calls function to create and append image
-        // displayMessage();
-      }
-    }, 1000);
+    winner.dataset.state = "visible";
+    winner.textContent = "You Win!🏆";
   }
-
-  function checkResult(){
-
+  else{
+    gLosses++;
+    winner.textContent = "";
   }
+  //statsTxt.textContent = "Stats:<br />Wins:" + gWins + "<br />Losses:" + gLosses + "<br />Tries:" + gTries;
+  document.querySelector('#win').textContent = "Wins: " + gWins;
+  document.querySelector('#loss').textContent = "Loses: " + gLosses;
+  document.querySelector('#tries').textContent = "Tries: " + gTries;
 
-  function getUserInput(){
-    
-  }
+}
 
-  countdown();
+// capture the key as the user is typing and compare with word.  
+//If a character amtch - add it to the screen at the correct position
+function getUserInput(event) {
+  var key = event.key;
+
+  console.log("in getuserinput");
+  event.preventDefault();
+    for (var i = 0; i < wArr.length; i++) {
+      if (key === wArr[i])
+        kArr[i] = key;
+    }
+    wordTxt.textContent = kArr.join("");
+
+    if(kArr.join('') === word){
+      timeLeft = 0;
+    }
+
+}
+
+timerBtn.addEventListener("click", countdown);
+
+window.addEventListener("keydown", getUserInput);
+
+
